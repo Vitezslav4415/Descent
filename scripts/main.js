3b289c62f4b6ec192527537b4e2680ab92f11a85
Vitezslav4415 <kosvic11@mail.ru>
2015-05-31 13:08:26 +0300

    Changed selects to bootstrap dropdown

--- scripts/main.js
+++ scripts/main.js
@@ -5,40 +5,50 @@ function toggleMenu() {
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 var mapWidth = 26;
-var mapHeight = 26;
+var mapHeigth = 26;
 
-function addOption(title, optionClass) {
-	return '<option class="' + optionClass + '">' + title + '</option>';
+function addOptionOld(title, value, optionClass) {
+	return '<option class="' + optionClass + '" value="' + value + '">' + title + '</option>';
+}
+
+function addOption(title, value, optionClass) {
+	return '<li class="' + optionClass + '"><a href="javascript:setOption(\'' + value + '\')">' + title + '</a></li>';
+}
+
+function getAlphabetChar(number) {
+	result = '';
+	if (number > 26) {
+		result += ALPHABET.charAt(Math.floor(number/26));
+	}
+	return result += ALPHABET.charAt(number%26);
 }
 
 function createXSelectContent () {
 	var html = '';
 	for (i = 1; i <= mapWidth; i++) {
-		html += addOption(i.toString(), 'oneCell');
+		html += addOption(i.toString(), '1' + i.toString(), 'oneCell');
 		if (i <= mapWidth-1)
-			html += addOption(i.toString() + '-' + (i+1).toString(), 'twoCells');
+			html += addOption(i.toString() + '-' + (i+1).toString(), '2' + i.toString(), 'twoCells');
 		if (i <= mapWidth-2)
-			html += addOption(i.toString() + '-' + (i+2).toString(), 'threeCells');
+			html += addOption(i.toString() + '-' + (i+2).toString(), '3' + i.toString(), 'threeCells');
 	}
 	return html;
 }
 
-function createYSelect (height, heightAlt) {
+function createYSelectContent () {
 	var html = '';
 	for (i = 1; i <= mapHeigth; i++) {
-		html += addOption(ALPHABET.charAt(i-1), 'oneCell');
+		html += addOption(getAlphabetChar(i-1), '1' + i.toString(), 'oneCell');
 		if (i <= mapHeigth-1)
-			html += addOption(ALPHABET.charAt(i-1) + '-' + ALPHABET.charAt(i), 'twoCells');
+			html += addOption(getAlphabetChar(i-1) + '-' + getAlphabetChar(i), '2' + i.toString(), 'twoCells');
 		if (i <= mapHeigth-2)
-			html += addOption(ALPHABET.charAt(i-1) + '-' + ALPHABET.charAt(i+1), 'threeCells');
+			html += addOption(getAlphabetChar(i-1) + '-' + getAlphabetChar(i+1), '3' + i.toString(), 'threeCells');
 	}
 	return html;
 }
 
 
 $(function() {
-	/*$('.menu').click(function() {
-		$(this).toggleClass('active');
-	});*/
-	$('.map-select').append(createXSelectContent(1));
+	$('.select-x ul').append(createXSelectContent());
+	$('.select-y ul').append(createYSelectContent());
 });
