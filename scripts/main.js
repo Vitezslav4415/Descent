ddbd5a4db57b59a9b334e593fafb651689473ec6
Vitezslav4415 <kosvic11@mail.ru>
2015-05-31 12:34:24 +0300

    Added functions to generate select content

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,44 @@
+function toggleMenu() {
+	$('.menu').toggleClass('active');
+}
+
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var mapWidth = 26;
+var mapHeight = 26;
+
+function addOption(title, optionClass) {
+	return '<option class="' + optionClass + '">' + title + '</option>';
+}
+
+function createXSelectContent () {
+	var html = '';
+	for (i = 1; i <= mapWidth; i++) {
+		html += addOption(i.toString(), 'oneCell');
+		if (i <= mapWidth-1)
+			html += addOption(i.toString() + '-' + (i+1).toString(), 'twoCells');
+		if (i <= mapWidth-2)
+			html += addOption(i.toString() + '-' + (i+2).toString(), 'threeCells');
+	}
+	return html;
+}
+
+function createYSelect (height, heightAlt) {
+	var html = '';
+	for (i = 1; i <= mapHeigth; i++) {
+		html += addOption(ALPHABET.charAt(i-1), 'oneCell');
+		if (i <= mapHeigth-1)
+			html += addOption(ALPHABET.charAt(i-1) + '-' + ALPHABET.charAt(i), 'twoCells');
+		if (i <= mapHeigth-2)
+			html += addOption(ALPHABET.charAt(i-1) + '-' + ALPHABET.charAt(i+1), 'threeCells');
+	}
+	return html;
+}
+
+
 $(function() {
-	$('.menu').click(function() {
+	/*$('.menu').click(function() {
 		$(this).toggleClass('active');
-	});
+	});*/
+	$('.map-select').append(createXSelectContent(1));
 });
